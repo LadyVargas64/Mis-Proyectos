78967e175443746e15b06872d2c88d6fac1075d4
LadyVargas64 <ladyvargas64@gmail.com>
2017-03-02 20:32:04 -0500

    Ejercicio 2

--- taller.py
+++ taller.py
@@ -2,7 +2,16 @@
 print("Numeros del 20 a 1 ")
 for i in range(20,0,-1):
     print(i)
-print('------------------')    
+print('------------------')
+#Segundo punto  
+print("Cadena de Texto contando las a")
+cadena = 'luz g3onz1ale1z'
+x = 0;
+for i in cadena:
+    if(i == 'z'):
+        x = x + 1
+print('La cadena "'+cadena+'" tiene '+(str(x))+' as')
+print('------------------')
 
     
    
