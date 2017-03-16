8a907f2de3eeef75f92907ca814a6ed1419fdc3f
LadyVargas64 <ladyvargas64@gmail.com>
2017-03-16 10:13:18 -0500

    Taller2.py
    Ejercicio 3 de 6, Saber el area de un triangulo

--- Python/taller2.py
+++ Python/taller2.py
@@ -15,3 +15,11 @@ elif(a > 500):
     print('Numero mayor a 500 y menor a 1000')
 elif(a < 500):
     print('Numero menor a 500') 
+#Tercer Punto
+print('----------------------------------------------------------')
+print('')
+print('~Saber el area de un triangulo')
+a=5
+b=6
+r=(a*b)/2
+print ("El area del Triangulo es" +str (r))
