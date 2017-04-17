f94515ad5282aa6df42a09ffec94931c05aaf7cf
LadyVargas64 <ladyvargas64@gmail.com>
2017-04-16 19:34:17 -0500

    rimas y multplo
    saber si dos palabras riman y si un numero es multiplo de otro

---
+++ Python/rimas.py
@@ -0,0 +1,19 @@
+a = 12
+b = 6
+
+c = a % b
+
+if(c == 0):
+    print('a es multiplo de b')
+else:
+    print('a no es multiplo de b')
+print('----------------------------------')    
+a = 'colores'
+b = 'mares'
+
+if(a[-3:] == b[-3:]):
+    print('Las palabras riman')
+else:
+    print('Las palabras no riman')
+
+    
